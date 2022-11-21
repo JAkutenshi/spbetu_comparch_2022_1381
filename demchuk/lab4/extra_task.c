010773e2ef2621013ec0e0fbef6400f14a7c3d10
pddemchuk <90906628+pddemchuk@users.noreply.github.com>
2022-11-22 00:35:40 +0300

    Update extra_task.c

--- demchuk/lab4/extra_task.c
+++ demchuk/lab4/extra_task.c
@@ -6,51 +6,47 @@ int main() {
     int char_max = 80;
     int counter = 0;
     char* input = (char*) calloc(char_max, sizeof(char));
-
     fgets(input, char_max, stdin);
     int len = strlen(input) - 1;
 
     puts(" Количество не_букв в строке.");
 
     asm("check_letter: \n"
-        "\t mov %%al, [%[inp]+%[idx]] \n"
-        "\t cmp %%al, 'A' \n"
+        "\t lodsb \n"
+        "\t cmp %%al, 208 \n"
+        "\t je rus \n"
+        "\t cmp %%al, 209 \n"
+        "\t je rus \n"
+        "\t cmp %%al, 65 \n"
         "\t jl count \n"
-        "\t cmp %%al, 'Z' \n"
+        "\t cmp %%al, 91 \n"
         "\t jl next \n"
-        "\t cmp %%al, 'a' \n"
+        "\t cmp %%al, 97 \n"
         "\t jl count \n"
         "\t cmp %%al, 123 \n"
         "\t jl next \n"
         "\t cmp %%al, 128 \n"
         "\t jl count \n"
-        "\t cmp %%al, 176 \n"
-        "\t jl next \n"
-        "\t cmp %%al, 224 \n"
-        "\t jl count \n"
-        "\t cmp %%al, 241 \n"
-        "\t jg count \n"
+        "\t jmp next \n"
+
+        "rus: \n"
+        "\t lodsb \n"
+        "\t sub cx, 1 \n"
         "\t jmp next \n"
 
         "count: \n"
         "\t inc %0 \n"
 
         "next: \n"
-        "\t inc %[idx] \n"
         "\t loop check_letter \n"
 
         "end: \n"
 
             : "+r" (counter)
-            : [inp] "b" (input),
-              [count] "c" (len),
-              [idx] "S" (0L)
+            : [inp] "S" (input),
+            "c" (len)
             : "ax"
     );
-
-    for (int i = 0; i < len; i++){
-        printf("%d %c\n", i, input[i]);
-    }
 
     printf("result %d\n", counter);
 
