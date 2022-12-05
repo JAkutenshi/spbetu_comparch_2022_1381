13208f6a8e16370fdf93c49f050ba6d63a3e620b
Tarasov Konstantin <tarasov02-03@mail.ru>
2022-12-06 02:55:25 +0300

    lab6: done

---
+++ lab6/main.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <fstream>
+#include <random>
+
+
+extern "C" void FUNC(int* array, int array_size, int* left_boarders, int intervals_size, int* result_array);
+
+int main()
+{
+	int Xmin, Xmax, NumRanDat, interval_amount;
+	int* left_borders;
+	int* arr;
+	int* result_array;
+	std::random_device rand;
+	std::mt19937 gen(rand());
+	std::ofstream file("out.txt");
+
+	std::cout << "Amount of numbers:" << std::endl;
+	std::cin >> NumRanDat;
+
+	std::cout << "Min: " << std::endl;
+	std::cin >> Xmin;
+	std::cout << "Max: " << std::endl;
+	std::cin >> Xmax;
+
+	while (Xmax < Xmin) {
+		std::cout << "Wrong!" << std::endl;
+		std::cout << "Min: " << std::endl;
+		std::cin >> Xmin;
+		std::cout << "Max" << std::endl;
+		std::cin >> Xmax;
+	}
+
+	std::cout << "Amount of intervals: " << std::endl;
+	std::cin >> interval_amount;
+
+	while (interval_amount <= 0)
+	{
+		std::cout << "Wrong!" << std::endl;
+		std::cout << "Amount of intervals: " << std::endl;
+		std::cin >> interval_amount;
+	}
+
+	left_borders = new int[interval_amount];
+	std::cout << "Enter left borders" << std::endl;
+	for (int i = 0; i < interval_amount; i++)
+		std::cin >> left_borders[i];
+
+	for (int i = 0; i < interval_amount - 1; i++)
+	{
+		for (int j = i + 1; j < interval_amount; j++)
+		{
+			if (left_borders[j] < left_borders[i])
+				std::swap(left_borders[j], left_borders[i]);
+		}
+	}
+
+	std::uniform_int_distribution<> dis(Xmin, Xmax);
+	arr = new int[NumRanDat];
+
+	for (int i = 0; i < NumRanDat; i++)
+		arr[i] = dis(gen);
+
+	std::cout << "Generated numbers: ";
+	file << "Generated numbers: ";
+	for (int i = 0; i < NumRanDat; i++) {
+		std::cout << arr[i] << ' ';
+		file << arr[i] << ' ';
+	}
+	std::cout << '\n';
+	file << '\n';
+
+	result_array = new int[interval_amount];
+	for (int i = 0; i < interval_amount; i++)
+		result_array[i] = 0;
+
+	FUNC(arr, NumRanDat, left_borders, interval_amount, result_array);
+
+	std::cout << "Interval index \tInterval left border \tAmount of numbers in interval" << '\n';
+	file << "Interval index \tInterval left border \tAmount of numbers in interval" << '\n';
+
+	for (int i = 0; i < interval_amount; i++)
+	{
+		std::cout << "\t" << i + 1 << "\t\t" << left_borders[i] << "\t\t\t" << result_array[i] << '\n';
+		file << "\t" << i + 1 << "\t\t" << left_borders[i] << "\t\t\t" << result_array[i] << '\n';
+	}
+
+	delete[] left_borders;
+	delete[] arr;
+	delete[] result_array;
+	file.close();
+	return 0;
+}
---
+++ lab6/mod.asm
@@ -0,0 +1,50 @@
+.586
+.MODEL FLAT, C
+.CODE
+FUNC PROC C array:dword, array_size:dword, left_borders:dword, interval_amount:dword, result_array:dword
+push ecx
+push esi
+push edi
+push eax
+push ebx
+
+mov ecx, array_size
+mov esi, array
+mov edi, left_borders
+mov eax, 0
+l1:
+	mov ebx, 0; обнуляем ebx для записи кол-ва интервалов
+	borders:
+ 		cmp ebx, interval_amount; Если ebx больше либо равно кол-ву интервалов
+		jge borders_exit; То выходим
+		push eax
+		mov eax, [esi+4*eax]; Кладём в eax элемент из array
+		cmp eax, [edi+4*ebx]; Сравниваем элемент массива и один из left_borders
+		pop eax
+		jl borders_exit; Если элемент меньше левой границы, то выходим
+		inc ebx; Берём следующий интервал
+		jmp borders
+	borders_exit:
+	dec ebx; Возвращаемся на предыдущий интервал
+
+	cmp ebx, -1; Если ebx равен -1, то элемент не принадлежит не одному из интервалов
+	je skip; Если ebx >= 0, то элемент принадлежит какому-либо интервалу 
+	mov edi, result_array; Соответствующий элемент результирующего массива увеличиваем на 1
+	push eax
+	mov eax, [edi+4*ebx]
+	inc eax
+	mov [edi+4*ebx], eax
+	pop eax
+	mov edi, left_borders
+	skip:
+	inc eax; Следующий элемент массива
+loop l1; Пока ecx(Размер массива) != 0
+
+pop ebx
+pop eax
+pop edi
+pop esi
+pop ecx
+ret
+FUNC ENDP
+END
