c478200a82263b9978952babada9a6e330b97dfd
mantsevat <55297427+mantsevat@users.noreply.github.com>
2022-12-06 12:20:16 +0300

    task2

---
+++ mantseva/lab6/task/lb6t.s
@@ -0,0 +1,33 @@
+.globl distribution
+.type distribution, @function
+
+distribution:
+
+main_loop:
+	mov eax, 0
+	internal_loop:
+		cmp eax, edx
+		jge exit
+		mov ebx, [rdi][rcx*4-4]
+		push rbx
+		and ebx, 0x1
+		cmp ebx,0
+		pop rbx
+		jne exit
+		cmp ebx, [rsi][rax*4]
+		jl exit
+		push rdx
+		dec edx
+		cmp eax, edx
+		pop rdx
+		je write
+		cmp ebx, [rsi][rax*4 +4]
+		jl write
+		inc eax
+		jmp internal_loop
+	write:
+		incq [r8 + 4*rax]
+	exit: 
+loop main_loop
+
+ret
