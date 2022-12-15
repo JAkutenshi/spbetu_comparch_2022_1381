e4fc65b00956abaaef7cb2a6c035ca3e62ef6e84
pddemchuk <90906628+pddemchuk@users.noreply.github.com>
2022-12-15 18:54:16 +0300

    Update func.s

--- demchuk/lab6/func.s
+++ demchuk/lab6/func.s
@@ -8,6 +8,7 @@
 # r9  res2
 
 func:
+	push rdx
 	sub edx, 1
 start:
 	mov eax, 1
@@ -22,10 +23,33 @@ next_border:
 	jmp next_border
 write:
 	incq [r8][rax*4-4]
+	add [r9][rax*4-4], ebx
 	jmp end
 write_last_interval:
 	incq [r8][rax*4]
+	add [r9][rax*4], ebx
 end:	
 	loop start
+	pop rdx
+	mov ecx, edx
+	
+srednee:
+	sub edx, edx
+	mov eax, [r9][rcx*4-4]		#summa
+	mov ebx, [r8][rcx*4-4]		#kol-vo chisel
+	cmp ebx, 0
+	je end2
+	cmp eax, 0
+	jl negative	
+	div ebx
+	jmp result
+negative:
+	neg eax
+	div ebx
+	neg eax	
+result:	
+	mov [r9][rcx*4-4], eax
+end2:
+	loop srednee	
 
 	ret
